edcbd27be258133a76644561475e887d7a6f94b0
JianiWuWSCW <wuj@wsc.school.nz>
2024-05-06 11:02:37 +1200

    change

---
+++ main.py
@@ -0,0 +1,8 @@
+# ask the user name 
+input("What's your name")
+#Ask your user their name using input()
+# ask your user to the quiz using print()
+#  what the quiz is about using print()
+# the first question using input()
+# the user the answer using print()
+# the user it’s the end and thank them for playing using print()
