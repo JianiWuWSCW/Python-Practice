9f5a5663f20d78fcf337277f6a914ac9e152bbb2
JianiWuWSCW <wuj@wsc.school.nz>
2024-05-06 11:38:38 +1200

    v1

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
-# ask the user name 
-input("What's your name")
-#Ask your user their name using input()
-# ask your user to the quiz using print()
-#  what the quiz is about using print()
-# the first question using input()
-# the user the answer using print()
-# the user it’s the end and thank them for playing using print()
+# Ask the user their name 
+name=input("What's your name?")
+# Greet the user and introduce the quiz
+print("Welcome to the quiz")
+print("This quiz is about capital cities about the world")
+# Ask the user a question
+input("")
+# Tell the user the answer
+print("")
+# End the quiz
+input("")
