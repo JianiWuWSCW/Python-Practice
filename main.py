ddad91788775cf9a9e57fcfc32548005a12fe644
JianiWuWSCW <wuj@wsc.school.nz>
2024-05-17 09:07:12 +1200

    change

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
+play="yes"
+
 score=0
+
+
 # Ask the user their name 
 name=input("What's your name?")
 # Greet the user and introduce the quiz
 print("Welcome to the quiz",name)
 print("This quiz is about capital cities about the world")
+
 # Ask the user a question
+while play == "yes":
+score=0
 answer=input(" What is the capital city of New Zealand?").upper()
 
 # Tell the user the answer
@@ -25,7 +32,7 @@ if answer == "Auckland".upper():
     print("correct!")
     score=score+10
 
-elif answer ==""
+elif answer =="":
     print("not sure???")
 else:
     print("incorrect")
@@ -50,9 +57,16 @@ else:
     print("incorrect")
     print("A")
 
+run = input("Do you want to go to Wellington?")
+
+while run=="yes":
+    print("Let's go!")
+    run=input("Do you want to go to Wellington?")
+
 
 # End the quiz
 
 print("Well done {}.You finished the quiz.You final score is{}".format(name,score))
 input("Goodbye!") 
-
+# Replay
+play = input("Do you want to play again?")
---
+++ testing.py
@@ -0,0 +1,19 @@
+score=0
+play="yes"
+
+# Ask the user their name
+name = input("What's your name?")
+
+# Greet the user and introduce the quiz
+print("Welcome to the quiz",name)
+print("This quiz is about capital cities about the world")
+
+
+
+while play == "yes":
+
+question="What's the capital city of New Zealand?"
+a="Wellington"
+b="Auckland"
+
+play=input("Do you want to play it again?")
