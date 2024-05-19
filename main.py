48e0b8b57e8f4c9d660620b0b5da0ac0735dde02
JianiWuWSCW <wuj@wsc.school.nz>
2024-05-20 11:26:23 +1200

    change

--- main.py
+++ main.py
@@ -11,62 +11,59 @@ print("This quiz is about capital cities about the world")
 
 # Ask the user a question
 while play == "yes":
-score=0
-answer=input(" What is the capital city of New Zealand?").upper()
+    score=0
+    answer=input(" What is the capital city of New Zealand?").upper()
 
-# Tell the user the answer
-if answer == "Wellington".upper():
-    print("correct!")
-    score= score+10
+    # Tell the user the answer
+    if answer == "Wellington".upper():
+        print("correct!")
+        score= score+10
 
-elif answer =="":
-    print("not sure???")
-else:
-    print("incorrect")
-    print("Wellington")
+    elif answer =="":
+        print("not sure???")
+    else:
+        print("incorrect")
+        print("Wellington")
 
 
-answer=input("What is the biggest city of New Zealand?").upper()
+    answer=input("What is the biggest city of New Zealand?").upper()
 
-if answer == "Auckland".upper():
-    print("correct!")
-    score=score+10
+    if answer == "Auckland".upper():
+        print("correct!")
+        score=score+10
 
-elif answer =="":
-    print("not sure???")
-else:
-    print("incorrect")
-    print("Auckland")
+    elif answer =="":
+        print("not sure???")
+    else:
+        print("incorrect")
+        print("Auckland")
 
-QUESTION_FORMAT = "{}\nA.{} B.{} C.{} D.{}"
-question = "What is the capital city of New Zealand?"
-a= "Wellington"
-b= "London"
-c= "Auckland"
-d= "Beijing"
-answer= input(QUESTION_FORMAT.format(question,a,b,c,d)).lower()
+    QUESTION_FORMAT = "{}\nA.{} B.{} C.{} D.{}"
+    question = "What is the capital city of New Zealand?"
+    a= "Wellington"
+    b= "London"
+    c= "Auckland"
+    d= "Beijing"
+    answer= input(QUESTION_FORMAT.format(question,a,b,c,d)).lower()
 
-if answer == "a".lower() or answer==a.lower():
-    print("correct!")
-    score=score+10
+    if answer == "a".lower() or answer==a.lower():
+        print("correct!")
+        score=score+10
 
-elif answer =="":
-    print("not sure???") 
-    print("Wellington")
-else:
-    print("incorrect")
-    print("A")
+    elif answer =="":
+        print("not sure???") 
+        print("Wellington")
+    else:
+        print("incorrect")
+        print("A")
 
-run = input("Do you want to go to Wellington?")
+    print("Well done {}.You finished the quiz.You final score is{}".format(name,score))
+    play = input("Do you want to do this quiz again?")
 
-while run=="yes":
-    print("Let's go!")
-    run=input("Do you want to go to Wellington?")
+
 
 
 # End the quiz
 
-print("Well done {}.You finished the quiz.You final score is{}".format(name,score))
-input("Goodbye!") 
-# Replay
-play = input("Do you want to play again?")
+print("Goodbye!") 
+
