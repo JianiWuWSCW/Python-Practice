18344f2f8fa0175d66a502356fa61d4a5db6f5d8
JianiWuWSCW <wuj@wsc.school.nz>
2024-06-20 09:27:00 +1200

    change

--- practice2.py
+++
@@ -1,8 +0,0 @@
-# Ask the user their name 
-name=input("What's your name?")
-# Greet the user and introduce the quiz
-print("Welcome to the Forest World!",name)
-
-print("背景：你是一個著名的出土文物研究人員，在一次考古中，
-      你與所在的考古團隊迷戀了，被困在了這篇森林中，到了傍晚，你們始終沒能找到出去的路,隨著夜幕降臨，森林中起了大霧，你們看不清彼此，也爲了取暖，大家只好縮在一起，等儅天亮起來你們打算繼續趕路，但是第二天起來，你發現大家都不見了
-      你將在一片森林中醒來，")
